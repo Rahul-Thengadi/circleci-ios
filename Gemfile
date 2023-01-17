dedc3851c8221bc7b53ba3e91c5b72baa59b899e
Rahul <thengadir@gmail.com>
2023-01-17 13:52:46 +0530

    inital commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'fastlane', '2.211.0'
+gem 'cocoapods', '1.11.3'
+gem 'bundler', '~> 2.0'
---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Rahul Thengadi on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
